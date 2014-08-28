5f5c0c25ae4ef0ec3f9614c33389a252b2aa70f5
Eric <guangqiang.dev@gmail.com>
2014-08-28 21:55:02 +0800

    Added a Cell CategoryCell.

---
+++ qiwenge/cells/CategoryCell.h
@@ -0,0 +1,16 @@
+//
+//  CategoryCell.h
+//  qiwenge
+//
+//  Created by Eric on 14-8-22.
+//  Copyright (c) 2014年 qiwenge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseCell.h"
+
+@interface CategoryCell : BaseCell
+
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
+
+@end
---
+++ qiwenge/cells/CategoryCell.m
@@ -0,0 +1,25 @@
+//
+//  CategoryCell.m
+//  qiwenge
+//
+//  Created by Eric on 14-8-22.
+//  Copyright (c) 2014年 qiwenge. All rights reserved.
+//
+
+#import "CategoryCell.h"
+
+@implementation CategoryCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
