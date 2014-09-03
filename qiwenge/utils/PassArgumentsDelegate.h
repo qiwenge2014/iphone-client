ba91547fa2336668d79b47dc1f090f9120e0e066
Eric <guangqiang.dev@gmail.com>
2014-09-03 22:19:42 +0800

    Create a protocol named PassArgumentsDelegate for passing arguments to other ViewController.

---
+++ qiwenge/utils/PassArgumentsDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PassArgumentsDelegate.h
+//  qiwenge
+//
+//  Created by Eric on 14-8-30.
+//  Copyright (c) 2014年 qiwenge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol PassArgumentsDelegate <NSObject>
+
+-(void)passArguments:(id)id;
+
+@end
