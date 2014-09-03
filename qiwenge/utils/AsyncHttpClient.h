915ad40d5f29c5741905d0ef9876fefe617b3b7d
Eric <guangqiang.dev@gmail.com>
2014-09-03 22:17:33 +0800

    Create AsyncHttpClient for Http request.

---
+++ qiwenge/utils/AsyncHttpClient.h
@@ -0,0 +1,13 @@
+//
+//  AsyncHttpClient.h
+//  qiwenge
+//
+//  Created by Eric on 14-8-28.
+//  Copyright (c) 2014年 qiwenge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AsyncHttpClient : NSObject
+
+@end
---
+++ qiwenge/utils/AsyncHttpClient.m
@@ -0,0 +1,13 @@
+//
+//  AsyncHttpClient.m
+//  qiwenge
+//
+//  Created by Eric on 14-8-28.
+//  Copyright (c) 2014年 qiwenge. All rights reserved.
+//
+
+#import "AsyncHttpClient.h"
+
+@implementation AsyncHttpClient
+
+@end
