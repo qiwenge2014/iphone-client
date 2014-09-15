d687e754ab0b3c29fca39791d617684b82a0166b
Eric <guangqiang.dev@gmail.com>
2014-09-15 12:00:24 +0800

    Added NSString+utlis and EGORefreshTableHeaderView

---
+++ qiwenge/additions/NSString+Utils.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Utils.h
+//  qiwenge
+//
+//  Created by Eric on 14-9-12.
+//  Copyright (c) 2014年 qiwenge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Utils)
+
+@end
---
+++ qiwenge/additions/NSString+Utils.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Utils.m
+//  qiwenge
+//
+//  Created by Eric on 14-9-12.
+//  Copyright (c) 2014年 qiwenge. All rights reserved.
+//
+
+#import "NSString+Utils.h"
+
+@implementation NSString (Utils)
+
+@end
