123a784173901efea564905df3b25e0d17a8b135
Eric <guangqiang.dev@gmail.com>
2014-09-28 10:20:20 +0800

    Updated.

--- qiwenge/BaseTableViewController.h
+++ qiwenge/BaseTableViewController.h
@@ -7,7 +7,38 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "EGORefreshTableHeaderView.h"
 
-@interface BaseTableViewController : UIViewController
+typedef enum {
+    Normal,
+    Refreshing,
+    Paging
+}LoadStatus;
+
+@interface BaseTableViewController : UIViewController<UITableViewDataSource,UITableViewDelegate,EGORefreshTableHeaderDelegate >
+
+@property (nonatomic, nonatomic) IBOutlet UITableView *mTableView;
+
+@property (strong, nonatomic) NSMutableArray *data;
+
+@property (nonatomic, strong) EGORefreshTableHeaderView *refreshHeaderView; 
+
+@property (nonatomic, assign) NSInteger limit;
+
+@property (nonatomic, assign) NSInteger pageindex;
+
+@property (nonatomic, assign) LoadStatus loadStatus;
+
+@property (nonatomic, assign) BOOL enableFooterPage;
+
+-(void)setEnablePullToRefresh:(BOOL)enable;
+
+-(void)setEnableFooterPage:(BOOL)enablePage;
+
+-(void)requestSuccess:(NSArray *)array;
+
+-(void)requestFailed;
+
+-(void)requestData;
 
 @end
--- qiwenge/BaseTableViewController.m
+++ qiwenge/BaseTableViewController.m
@@ -26,7 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.data =[[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,15 +35,121 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)setEnableFooterPage:(BOOL)enablePage{
+    _enableFooterPage=enablePage;
 }
-*/
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    int count=self.data.count;
+    if(_enableFooterPage)count=count+1;
+    return count;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (_enableFooterPage&&indexPath.row==self.data.count) {
+        return 44;
+    }
+    UITableViewCell *cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
+    return cell.frame.size.height;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (_enableFooterPage&&indexPath.row==self.data.count) {
+        FooterPageCell *pageCell=[tableView dequeueReusableCellWithIdentifier:[FooterPageCell getCellReuseIdentifier]];
+        if(!pageCell){
+            NSLog(@"create footer");
+            pageCell=[FooterPageCell getCellFromXib];
+        }
+        return pageCell;
+    }
+    return nil;
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    [self.refreshHeaderView egoRefreshScrollViewDidScroll:scrollView];
+    [self checkLoadMore];
+}
+
+-(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    [self.refreshHeaderView egoRefreshScrollViewDidEndDragging:scrollView];
+}
+
+-(void)checkLoadMore{
+    if(self.enableFooterPage&&self.loadStatus==Normal&&self.mTableView.contentOffset.y >= (self.mTableView.contentSize.height - self.mTableView.bounds.size.height)){
+        [self loadMore];
+    }
+}
+
+//－－－－－－－－－－－－－－－pullToRefresh start－－－－－－－－－－－－－－－
+
+-(void)setEnablePullToRefresh:(BOOL)enable{
+    if (enable) {
+        CGRect tableViewBounds = self.mTableView.bounds;
+        self.refreshHeaderView = [[EGORefreshTableHeaderView alloc]
+                                  initWithFrame:CGRectMake(0, 0 - tableViewBounds.size.height, tableViewBounds.size.width, tableViewBounds.size.height)
+                                  arrowImageName:@"icon_pull_to_refresh"
+                                  textColor:[UIColor lbTitleColor]];
+        self.refreshHeaderView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
+        self.refreshHeaderView.backgroundColor = [UIColor backgoundColor];
+        self.refreshHeaderView.delegate = self;
+        self.refreshHeaderView.pullingTitle=@"释放刷新";
+        self.refreshHeaderView.normalTitle=@"下拉刷新";
+        self.refreshHeaderView.LoadingTitle=@"正在加载";
+        [self.mTableView addSubview:self.refreshHeaderView];
+    }else if(self.refreshHeaderView){
+        [self.refreshHeaderView removeFromSuperview];
+        self.refreshHeaderView=nil;
+    }
+}
+
+- (BOOL)egoRefreshTableHeaderDataSourceIsLoading:(EGORefreshTableHeaderView *)view {
+    return self.loadStatus != Normal;
+}
+
+-(void)egoRefreshTableHeaderDidTriggerRefresh:(EGORefreshTableHeaderView *)view {
+    [self refreshStart];
+}
+
+//－－－－－－－－－－－－－－－pullToRefresh end－－－－－－－－－－－－－－－
+
+-(void)loadMore{
+    self.loadStatus=Paging;
+    _pageindex++;
+    [self requestData];
+}
+
+-(void)refreshStart{
+    if (self.loadStatus == Normal) {
+        self.pageindex=1;
+        self.loadStatus = Refreshing;
+        [self requestData];
+    }
+}
+
+-(void)refreshFinished{
+    self.loadStatus=Normal;
+    [self.refreshHeaderView egoRefreshScrollViewDataSourceDidFinishedLoading:self.mTableView];
+}
+
+-(void)requestSuccess:(NSArray *)array{
+    NSLog(@"requestSuccess");
+    [self refreshFinished];
+    
+    if (self.pageindex==1) {
+        [self.data removeAllObjects];
+    }
+    [self.data addObjectsFromArray:array];
+    
+    [self.mTableView reloadData];
+}
+
+-(void)requestFailed{
+     NSLog(@"requestFailed");
+    [self refreshFinished];
+}
+
+-(void)requestData{
+    NSLog(@"requestData");
+}
 
 @end
