54f0bb0a5e7ae55dc264a7a924d0a192eb10ae60
Eric <guangqiang.dev@gmail.com>
2015-03-05 23:13:27 +0800

    Created MeViewController for showing user's information after sign in.

---
+++ qiwenge/MeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MeViewController.h
+//  qiwenge
+//
+//  Created by Eric on 15/2/18.
+//  Copyright (c) 2015年 qiwenge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeViewController : UIViewController
+
+@end
